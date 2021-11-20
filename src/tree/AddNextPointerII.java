38855b1963a2ce3acf5c0b48a00aacc16a216b5a
ST Wang <stefanwang2020@gmail.com>
2021-11-19 18:39:46 -0800

    Populating Next Right Pointers in Each Node II

--- src/tree/AddNextPointerII.java
+++ src/tree/AddNextPointerII.java
@@ -1,4 +1,37 @@
 package tree;
 
 public class AddNextPointerII {
+    public TreeNode connect(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+
+        if(root.left == null && root.right == null){
+            return root;
+        }
+
+        TreeNode res = root;
+        while(root != null){
+            TreeNode dummy = new TreeNode();
+            TreeNode tmp = dummy;
+
+            while(root != null){
+                if(root.left != null){
+                    tmp.next = root.left;
+                    tmp = tmp.next;
+                    // 必须保证 root 有左、右孩子的时候才可以进行 tmp = tmp.next
+                }
+
+                if(root.right != null){
+                    tmp.next = root.right;
+                    tmp = tmp.next;
+                }
+
+                root = root.next;
+            }
+            root = dummy.next;
+            // dummy.next 已经连到了 root的某个子树上
+        }
+        return res;
+    }
 }
